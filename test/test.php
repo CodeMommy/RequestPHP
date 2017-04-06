5ecd0ca37a5327b8ddb17f79a6a3a4d37f87ae43
Candison <kandisheng@163.com>
2017-04-06 16:54:54 +0800

    0.0.1

--- test/test.php
+++ test/test.php
@@ -4,10 +4,6 @@
  * @author   Candison November <www.kandisheng.com>
  */
 
-require_once(__DIR__ . '/../source/Request.php.php');
+require_once(__DIR__ . '/../source/Request.php');
 
 use CodeMommy\RequestPHP\Request;
-
-Request::setRoot('./');
-$result = Request::get('application.config.test.hello', 'default');
-var_dump($result);
